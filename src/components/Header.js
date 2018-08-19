08390e161ba03e456cb70aa43be7ebd8aee81124
soronto <soronto3603@gmail.com>
2018-08-20 01:21:20 +0900

    header test

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    const style={
+      backgroundColor:'black',
+      padding:'16px',
+      color:'white',
+      fontSize:'12px'
+    };
+    return (
+      <div style={style}>
+      Header
+      </div>
+    );
+  }
+}
+
+export default Header;
