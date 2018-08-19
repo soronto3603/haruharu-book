9c81660036956ab095b344fa198439b2a16442a3
soronto <soronto3603@gmail.com>
2018-08-20 01:30:30 +0900

    add header

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-// import Header from './components/Header'
+import Header from './components/Header'
 
-// ReactDOM.render(<Header />,document.getElementById('header'));
+ReactDOM.render(<Header />,document.getElementById('header'));
 ReactDOM.render(<App />, document.getElementById('root'));
 
 registerServiceWorker();
