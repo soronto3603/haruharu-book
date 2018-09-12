70bd7c212e2f79884e7c6a486577a6887a645b8b
soronto <soronto3603@gmail.com>
2018-09-12 21:36:43 +0900

    2018-09-12-before-db-plugin-test

--- haruharu-book-app/www/js/index.js
+++ haruharu-book-app/www/js/index.js
@@ -1,22 +1,37 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+function openPage(pagename,child_tab){
+  var pagecontent=document.getElementsByClassName("pagecontent");
+  for(var i=0;i<pagecontent.length;i++){
+    pagecontent[i].style.display="none";
+  }
+  if(pagename=="Main"){
 
+  }else if(pagename=="Temp"){
+
+  }
+  document.getElementById(pagename).style.display="block";
+  openTab(child_tab);
+}
+function openTab(tabName){
+  var tabcontent=document.getElementsByClassName("tabcontent");
+  for(var i=0;i<tabcontent.length;i++){
+    tabcontent[i].style.display="none";
+  }
+  if(tabName=="Profile"){
+    console.log("Profile Event")
+    onLoadProfile();
+  }
+  document.getElementById(tabName).style.display="block";
+}
+var onLoadProfile=()=>{
+  var target=document.getElementById("My-achievement");
+  var red=30;
+  var yellow=20;
+  var green=50;
+  var html="<div class='red' style='width:"+30+"%'></div>";
+  html+="<div class='yellow' style='width:"+20+"%'></div>";
+  html+="<div class='green' style='width:"+50+"%'></div>";
+  target.innerHTML=html;
+}
 var app = {
     // Application Constructor
 
@@ -29,6 +44,9 @@ var app = {
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
+      // Start page
+      // openPage("Main","Home");
+      openPage("Temp","AddBook");
     }
 };
 
